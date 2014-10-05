6b2ddf49464d2c3723149719fda7704d66e51c2f
Michal Jurosz <mj@mj41.cz>
2014-10-05 13:16:26 +0200

    svn up -> git pull

--- ttclient-start.sh
+++ ttclient-start.sh
@@ -1,9 +1,10 @@
+#!/bin/bash
 
 for ((i=1;1;i++)); do
     perl ./ttclient.pl $@
     if [ ! -f './.do_ttclient_upgrade' ]; then
         exit;
     fi
-    echo "Trying to do client upgrade with 'svn up ..'."
-    svn up .. || exit
+    echo "Trying to do client upgrade with 'git pull'."
+    git pull || exit
 done
